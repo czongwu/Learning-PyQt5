4f2a6b6599352da7c002365cc2291321ef5ef380
czongwu <czongwu1@163.com>
2019-10-28 14:13:30 +0800

    MD-1

--- MD/MD_1.py
+++ MD/MD_1.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-from MD import MD_Mian
+from MD import MD_win
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainwindow = QWidget()
-    ui = MD_Mian.Ui_Form()
-    ui.setupUi(mainwindow)
+    mainwindow.setWindowIcon(QIcon('./img/konglong.ico'))
+    ui = MD_win.MD_window()
     mainwindow.show()
     sys.exit(app.exec_())
