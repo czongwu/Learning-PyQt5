f554e8452afc2cab76db0f5f756eed9b4e6d7d86
czongwu <czongwu1@163.com>
2019-10-28 10:34:13 +0800

    Tooltip

--- Tooltip.py
+++ Tooltip.py
@@ -1,4 +1,4 @@
-# -*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
@@ -7,15 +7,15 @@ from PyQt5.QtGui import *
 class Tooltip(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.button1 = QPushButton('My Button')
         self.initUI()
 
     def initUI(self):
         QToolTip.setFont(QFont('SansSerif', 12))
-        self.setToolTip('今天是<b>星期六</b>')
+        self.setToolTip('今天是<b>星期五</b>')
         self.setGeometry(300, 300, 200, 300)
         self.setWindowTitle('设置控件提示信息')
 
-        self.button1 = QPushButton('My Button')
         self.button1.setToolTip('这是一个按钮，Are you ok?')
         layout = QHBoxLayout()
         layout.addWidget(self.button1)
@@ -23,6 +23,7 @@ class Tooltip(QMainWindow):
         mainFrame = QWidget()
         mainFrame.setLayout(layout)
         self.setCentralWidget(mainFrame)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
