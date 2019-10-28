6dbfbde95edd85b6738513d1f6d9834664013408
czongwu <czongwu1@163.com>
2019-10-28 10:39:35 +0800

    Md-main

--- MD_Mian.py
+++ MD_Mian.py
@@ -2,11 +2,13 @@
 
 # Form implementation generated from reading ui file 'MD_Mian.ui'
 #
-# Created by: PyQt5 UI code generator 5.9.2
+# Created by: PyQt5 UI code generator 5.13.0
 #
 # WARNING! All changes made in this file will be lost!
 
+
 from PyQt5 import QtCore, QtGui, QtWidgets
+
 
 class Ui_Form(object):
     def setupUi(self, Form):
@@ -39,4 +41,3 @@ class Ui_Form(object):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Form"))
         self.pushButton.setText(_translate("Form", "PushButton"))
-
